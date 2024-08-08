5c5ee2e1912c66a95621bcd3410d7e610820dfe9
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2024-08-08 11:24:04 +0300

    Children Components added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,60 @@
-import { HeartIcon } from "@heroicons/react/24/outline"
+import { HeartIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import Modal from "./Modal";
+import { Character } from "./CharacterList";
 
-
-
-
-
-function Navbar() {
+export default function Navbar({ children }) {
   return (
     <nav className="navbar">
-        <div className="navbar__logo">LOGO</div>
-        <input type="text" className="text-field" placeholder="search..."/>
-        <div className="navbar__result">Found X characters</div>
-        <button className="heart">
-        <HeartIcon className="icon"/>
-        <span className="badge">4</span>
-        </button>
+      <Logo />
+      {children}
     </nav>
-  )
+  );
 }
 
-export default Navbar
+function Logo() {
+  return <div className="navbar__logo">LOGO 😍</div>;
+}
 
+export function Search({ query, setQuery }) {
+  return (
+    <input
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      type="text"
+      className="text-field"
+      placeholder="search..."
+    />
+  );
+}
+
+export function SearchResult({ numOfResult }) {
+  return <div className="navbar__result">Found {numOfResult} characters</div>;
+}
+
+export function Favourites({ favourites, onDeleteFavourite }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Modal onOpen={setIsOpen} open={isOpen} title="List of Favourites">
+        {favourites.map((item) => (
+          <Character key={item.id} item={item}>
+            <button
+              className="icon red"
+              onClick={() => onDeleteFavourite(item.id)}
+            >
+              <TrashIcon />
+            </button>
+          </Character>
+        ))}
+      </Modal>
+      <button className="heart" onClick={() => setIsOpen((is) => !is)}>
+        <HeartIcon className="icon" />
+        <span className="badge">{favourites.length}</span>
+      </button>
+    </>
+  );
+}
+
+// App => Navbar => SearchResult
